61cb675801dc958f74ed827957e9ed7a0b7b6141
Xinshuang Jin <jinxinshuang@gmail.com>
2025-05-19 16:47:47 -0400

    2025/05/19 Activity

--- two_sum.py
+++ two_sum.py
@@ -16,10 +16,17 @@ Adapted from: https://leetcode.com/problems/two-sum/
 """
 
 def two_sum(nums, target):
-    pass
+    for i in range(len(nums)-1):
+        for j in range(i+1, len(nums)):
+            if nums[i] + nums[j] == target:
+                return [i, j]
+    
+    return []
+
 
 assert two_sum([2, 7, 11, 15], 9) == [0, 1]
 assert two_sum([5, 7, 3, 15], 22) == [1, 3]
 assert two_sum([1, 2], 3) == [0, 1]
 assert two_sum([5, 4, 6], 22) == []
+assert two_sum([5, -15, 6], -10) == [0, 1]
 print("All tests passed!")
